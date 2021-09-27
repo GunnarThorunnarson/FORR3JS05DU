e544192bbcf16510c328f25516b72546d84cf021
Gunnar Þórunnarson <gus@tskoli.is>
2021-09-27 16:28:01 +0000

    Update staticFilter.js

--- efnistok/staticFilter.js
+++ efnistok/staticFilter.js
@@ -1,7 +1,6 @@
 /*
-Static filtering		
-	Verkefnið: Að finna einstaklinga sem rukka milli $65 og $90 og bæta þeim við nýtt fylki.
-	Sýnidæmi (jQuery útfærsla með töflu): http://javascriptbook.com/code/c12/filter-filter.html
+ * Að finna einstaklinga sem rukka milli $65 og $90 og bæta þeim við nýtt fylki.
+ * Sýnidæmi (jQuery útfærsla með töflu): http://javascriptbook.com/code/c12/filter-filter.html
 */  
 
 // DATA, Array of objects.
@@ -12,26 +11,17 @@ const people = [
 		{ name:'Nigel', rate: 120 }
 ];
        
-/* 
-	 The filter() array method creates a new array with all elements that pass the test implemented by the provided function.
-	 filter notar callback (kall á fallið priceRange) á hvert object í fylkinu people.
-	 Ef priceRange fallið skilar true þá er object úr people afritað í results fylkið.
-*/ 
 
-// results er fylki sem geymir síuð gögn, person er object í fylkinu people
-let results = people.filter(person => {
-	return (person.rate >= 65) && (person.rate <= 90); 	// skilar true ef skilyrði eru uppfyllt
-}) 
+function priceRange(person) {        
+    	// skilar true ef skilyrði eru uppfyllt
+		return (person.rate >= 65) && (person.rate <= 90); 	
+	};
+
+// filter fylkjaaðferðin, býr til nýtt fylki (results), með öllum stökum sem uppfylla skilyrði fallsins priceRange.
+// priceRange fallið er keyrt á hvert stak í fylkinu people.
+// Ef priceRange skilar true, þá er object vísun afrituð í results fylki, annars ekki ef það er false.
+let results = people.filter(priceRange); 
 
 console.log(people);  	// [{ name: 'Casey', rate: 60 },{ name: 'Camille', rate: 80 },{ name: 'Gordon', rate : 75 },{ name : 'Nigel', rate: 120 }]
 console.log(results); 	// [{ name: 'Camille', rate: 80 },{ name: 'Gordon', rate : 75 }]
 
-
-/* 
-	// önnur kóðaritun
-	let results = people.filter(priceRange); 	
-	
-	function priceRange(person) {        
-		return (person.rate >= 65) && (person.rate <= 90); 	
-	};
-*/
